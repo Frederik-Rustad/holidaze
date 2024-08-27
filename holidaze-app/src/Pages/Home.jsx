eafd5d0b43c27028f5a84c84b723431a13040fef
Frederik-Rustad <frederik.rustad.89@gmail.com>
2024-08-27 16:15:56 +0200

    p1
    p1

---
+++ holidaze-app/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Home = () => {
+
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Welcome to Holidaze Booking</p>
+    </div>
+  );
+}
+
+
+export default Home;
---
+++ holidaze-app/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Holidaze Booking</h1>
+      <nav>
+        <ul>
+          <li><a href="#search">Search</a></li>
+          <li><a href="#destinations">Destinations</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
