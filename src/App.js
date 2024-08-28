5374d13fe52c76dcd24d2ab7ec61c63926ff1f90
Frederik-Rustad <frederik.rustad.89@gmail.com>
2024-08-28 21:22:06 +0200

    directory placement
    directory placement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Home from './Pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+      <Home />
+      <Footer />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +24,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div>
       <h2>Home</h2>
-      <p>Welcome to Holidaze Booking</p>
+      <p>Welcome to Holidaze Booking </p>
     </div>
   );
 }
